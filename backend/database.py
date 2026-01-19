5aa760db368e25b616f7ddc3e32e19d7eea1947f
yutanatana <tokai32tanatana@gmail.com>
2026-01-19 23:01:53 +0900

    import文を追加

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.pool import NullPool
+import sqlalchemy_libsql
 import os
 import sys
 
